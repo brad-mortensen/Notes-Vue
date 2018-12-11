11d4a8d651c737734d14d2879714ce76a9dae0c3
Brad Mortensen <b.mort86@gmail.com>
2018-12-11 12:28:07 -0700

    Axios Get and Post Working

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import NotesList from '../components/NotesList.vue'
+import AddForm from '../components/AddForm.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/notes',
-      name: 'notes',
-      component: NotesList
-    }
+    {path: '/notes', name: 'notes', component: NotesList},
+    {path: '/addnote', name: 'add', component: AddForm}
   ]
 })
