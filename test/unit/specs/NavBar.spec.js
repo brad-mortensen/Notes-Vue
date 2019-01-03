06463697148b7c6d598bc036501dfacb932b996a
Brad Mortensen <b.mort86@gmail.com>
2019-01-03 12:35:54 -0700

    Unit Test Update

--- test/unit/specs/NavBar.spec.js
+++ test/unit/specs/NavBar.spec.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import NotesList from '@/components/NotesList'
 
-describe('NavBar.vue', () => {
+describe('NotesList.vue', () => {
   it('should render correct contents', () => {
     const vm = new Vue(NotesList).$mount()
     expect(vm.sortedAZ).toBe(false)
