0ec49debf802a2a9b34067b965dc5cb93f885c99
Brad Mortensen <b.mort86@gmail.com>
2019-01-21 15:18:05 -0700

    builds app

--- server.js
+++ server.js
@@ -5,4 +5,7 @@ const app = express();
 app.use(express.static(__dirname + "/dist/"));
 app.get(/.*/, (req, res)=>{
   res.sendfile(__dirname + "/dist/index.html")
-})
+});
+app.listen(port);
+
+console.log('Server started ...')
