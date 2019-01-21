fa136771344f13294dd731ba41d41378e9f7f654
Brad Mortensen <b.mort86@gmail.com>
2019-01-21 15:15:45 -0700

    adds redirects to server.js

--- server.js
+++ server.js
@@ -3,3 +3,6 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.static(__dirname + "/dist/"));
+app.get(/.*/, (req, res)=>{
+  res.sendfile(__dirname + "/dist/index.html")
+})
