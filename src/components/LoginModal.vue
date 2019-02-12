17745ab574f5d00ff34d22a53a22b9a3879f238f
Brad Mortensen <b.mort86@gmail.com>
2019-02-11 18:16:47 -0700

    error message

--- src/components/LoginModal.vue
+++ src/components/LoginModal.vue
@@ -38,7 +38,7 @@ export default {
           this.$router.push("/notes");
         })
         .catch(err => {
-          console.log("Login Error", err);
+          console.log(err);
         });
     }
   }
